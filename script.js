41f5757fa12ac0b43c1fcdeb646f4aa7f5fe37e5
unknown <Jarek@Jarek-PC.(none)>
2012-03-19 20:16:39 +0100

    Next commit

--- script.js
+++ script.js
@@ -6,17 +6,55 @@ var currentPlayerLeftPerc = 40;
 function Start()
 {
 	AddEventListeners();
+	setTimeout(function () { GenerateBomb(); }, 3000);
 	MainLoop();
+}
+
+function GenerateBomb()
+{
+	setTimeout(function () { GenerateBomb(); }, 3000);
+	
+	var bomb1 = document.getElementById("bomb1");
+	var bomb2 = document.getElementById("bomb2");
+	var bomb3 = document.getElementById("bomb3");
+	var bomb4 = document.getElementById("bomb4");
+	var bomb5 = document.getElementById("bomb5");
+	
+	if (bomb1.style.top == "1%")
+	{
+		bomb1.style.left = Math.floor(Math.random() * 101) + "%";
+		return;
+	}
+	else if (bomb2.style.top == "0%")
+	{
+		bomb2.style.left = Math.floor(Math.random() * 101) + "%";
+		return;
+	}
+	else if (bomb3.style.top == "0%")
+	{
+		bomb3.style.left = Math.floor(Math.random() * 101) + "%";
+		return;
+	}
+	else if (bomb4.style.top == "0%")
+	{
+		bomb4.style.left = Math.floor(Math.random() * 101) + "%";
+		return;
+	}
+	else if (bomb5.style.top == "0%")
+	{
+		bomb5.style.left = Math.floor(Math.random() * 101) + "%";
+		return;
+	}
 }
 
 function MainLoop()
 {
-	setTimeout(function () { MainLoop() }, 42);
+	setTimeout(function () { MainLoop(); }, 42);
 	
 	switch (message)
 	{
 		case Message.GoRight:
-			if (currentPlayerLeftPerc < 90)
+			if (currentPlayerLeftPerc < 79)
 				currentPlayerLeftPerc++;
 		break;
 		case Message.GoLeft:
